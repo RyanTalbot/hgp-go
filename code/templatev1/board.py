8efe55ba44fd8509e9f466e46371df375e2fcc92
Osebor Evbodaghe <osebor.evbodaghe@student.griffith.ie>
2022-01-04 13:19:13 +0000

    Method to check if the board has been filled

--- code/templatev1/board.py
+++ code/templatev1/board.py
@@ -1,111 +1,198 @@
-from PyQt5.QtWidgets import QFrame
-from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal, QPoint
-from PyQt5.QtGui import QPainter
+import math
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from piece import Piece
 
-class Board(QFrame):  # base the board on a QFrame widget
-    updateTimerSignal = pyqtSignal(int) # signal sent when timer is updated
-    clickLocationSignal = pyqtSignal(str) # signal sent when there is a new click location
 
-    # TODO set the board width and height to be square
-    boardWidth  = 0     # board is 0 squares wide # TODO this needs updating
-    boardHeight = 0     #
-    timerSpeed  = 1     # the timer updates ever 1 second
-    counter     = 10    # the number the counter will count down from
+# TODO - Show how much territory is controlled by a player
+# TODO - Allow a player to pass
+# TODO - Allow the game to be reset
+# TODO - Implement placement of stones in valid locations only
+# TODO - Implement capture of stone(s)
+
+
+class Board(QFrame):  # base the board on a QFrame widget
+    updateTimerSignal = pyqtSignal(int)  # signal sent when timer is updated
+    clickLocationSignal = pyqtSignal(str)  # signal sent when there is a new click location
+    changePlayerTurnSignal = pyqtSignal(int)  # signal sent when swap player is updated.
+
+    boardWidth = 7  # board is 6 squares wide
+    boardHeight = 7  # board is 6 squares tall
+    timerSpeed = 1000  # the timer updates ever 1 second
+    counter = 300  # the number the counter will count down from 300
+    playerTurn = Piece.Black  # starting player is always black
 
     def __init__(self, parent):
         super().__init__(parent)
         self.initBoard()
 
     def initBoard(self):
-        '''initiates board'''
+        """initiates board"""
         self.timer = QBasicTimer()  # create a timer for the game
-        self.isStarted = False      # game is not currently started
-        self.start()                # start the game which will start the timer
+        self.isStarted = False  # game is not currently started
+        self.start()  # start the game which will start the timer
 
-        self.boardArray =[]         # TODO - create a 2d int/Piece array to store the state of the game
-        # self.printBoardArray()    # TODO - uncomment this method after create the array above
+        # use INT type for now in array
+        self.boardArray = [[0 for i in range(7)] for j in range(7)]
+        self.printBoardArray()
 
     def printBoardArray(self):
-        '''prints the boardArray in an attractive way'''
+        """prints the boardArray in an attractive way"""
         print("boardArray:")
         print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.boardArray]))
 
     def mousePosToColRow(self, event):
-        '''convert the mouse click event to a row and column'''
+        """convert the mouse click event to a row and column"""
+        # Save the x,y position of each click
+        x_pos = event.x()
+        y_pos = event.y()
+
+        x_coord = round(x_pos / self.squareWidth())
+        y_coord = round(y_pos / self.squareHeight())
+
+        print(x_coord, y_coord)
+
+        # Adding a check system to ensure no invalid moves
+        if self.boardArray[y_coord][x_coord] == 0:
+            self.boardArray[y_coord][x_coord] = self.playerTurn
+            self.changePlayerTurn()
+        else:
+            self.showNotification("Invalid Move!")
+
+        self.printBoardArray()
+        self.update()
 
     def squareWidth(self):
-        '''returns the width of one square in the board'''
+        """returns the width of one square in the board"""
         return self.contentsRect().width() / self.boardWidth
 
     def squareHeight(self):
-        '''returns the height of one square of the board'''
+        """returns the height of one square of the board"""
         return self.contentsRect().height() / self.boardHeight
 
     def start(self):
-        '''starts game'''
-        self.isStarted = True                       # set the boolean which determines if the game has started to TRUE
-        self.resetGame()                            # reset the game
-        self.timer.start(self.timerSpeed, self)     # start the timer with the correct speed
+        """starts game"""
+        self.isStarted = True  # set the boolean which determines if the game has started to TRUE
+        self.resetGame()  # reset the game
+        self.timer.start(self.timerSpeed, self)  # start the timer with the correct speed
         print("start () - timer is started")
 
     def timerEvent(self, event):
-        '''this event is automatically called when the timer is updated. based on the timerSpeed variable '''
-        # TODO adapter this code to handle your timers
+        """this event is automatically called when the timer is updated. based on the timerSpeed variable """
         if event.timerId() == self.timer.timerId():  # if the timer that has 'ticked' is the one in this class
-            if Board.counter == 0:
-                print("Game over")
-            self.counter -= 1
-            print('timerEvent()', self.counter)
+            if self.counter == 0:
+                self.showNotification("Time's Up, Game Over!")
+                # update when game logic is finished top show winner
+                # if black wins print  self.show_notification("Black WINS")
+                # if white wins print  self.show_notification("White WINS")
+                self.timer.stop()
+                self.close()
+                self.resetGame()
+            else:
+                self.counter -= 1
+            # print('timerEvent()', self.counter)
             self.updateTimerSignal.emit(self.counter)
         else:
-            super(Board, self).timerEvent(event)      # if we do not handle an event we should pass it to the super
-                                                        # class for handelingother wise pass it to the super class for handling
+            super(Board, self).timerEvent(event)  # if we do not handle an event we should pass it to the super
+            # class for handling other wise pass it to the super class for handling
 
     def paintEvent(self, event):
-        '''paints the board and the pieces of the game'''
-        # painter = QPainter(self)
-        # self.drawBoardSquares(painter)
-        # self.drawPieces(painter)
+        """paints the board and the pieces of the game"""
+        painter = QPainter(self)
+        self.drawBoardSquares(painter)
+        self.drawPieces(painter)
 
     def mousePressEvent(self, event):
-        '''this event is automatically called when the mouse is pressed'''
-        clickLoc = "click location ["+str(event.x())+","+str(event.y())+"]"     # the location where a mouse click was registered
-        print("mousePressEvent() - "+clickLoc)
-        # TODO you could call some game logic here
+        """this event is automatically called when the mouse is pressed"""
+        clickLoc = "click location [" + str(event.x()) + "," + str(
+            event.y()) + "]"  # the location where a mouse click was registered
+        print("mousePressEvent() - " + clickLoc)
         self.clickLocationSignal.emit(clickLoc)
+        self.mousePosToColRow(event)
 
     def resetGame(self):
-        '''clears pieces from the board'''
-        # TODO write code to reset game
+        """clears pieces from the board"""
+        self.boardArray = [[0 for i in range(7)] for j in range(7)]
+        # set game pieces back to zero in game logic
 
     def tryMove(self, newX, newY):
-        '''tries to move a piece'''
+        """tries to move a piece"""
 
     def drawBoardSquares(self, painter):
-        '''draw all the square on the board'''
-        # TODO set the default colour of the brush
-        for row in range(0, Board.boardHeight):
-            for col in range (0, Board.boardWidth):
+        """draw all the square on the board"""
+        brush = QBrush(Qt.SolidPattern)
+        brush.setColor(QColor.fromRgb(217, 179, 255))  # test color
+        painter.setBrush(brush)
+
+        for row in range(0, Board.boardHeight - 1):
+
+            if brush.color() == (QColor.fromRgb(217, 179, 255)):  # to ensure alternate colors on new columns
+                brush.setColor(QColor.fromRgb(0, 153, 153))
+            else:
+                brush.setColor(QColor.fromRgb(217, 179, 255))
+
+            for col in range(0, Board.boardWidth - 1):
                 painter.save()
-                colTransformation = self.squareWidth()* col # TODO set this value equal the transformation in the column direction
-                rowTransformation = 0                       # TODO set this value equal the transformation in the row direction
-                painter.translate(colTransformation,rowTransformation)
-                painter.fillRect()                          # TODO provide the required arguments
+                colTransformation = self.squareWidth() * col
+                rowTransformation = self.squareHeight() * row
+                painter.translate(colTransformation, rowTransformation)
+                painter.fillRect(35, 35, self.squareWidth(), self.squareHeight(), brush)
                 painter.restore()
-                # TODO change the colour of the brush so that a checkered board is drawn
+
+                if brush.color() == (QColor.fromRgb(217, 179, 255)):  # to ensure alternate colors on new columns
+                    brush.setColor(QColor.fromRgb(0, 153, 153))
+                else:
+                    brush.setColor(QColor.fromRgb(217, 179, 255))
 
     def drawPieces(self, painter):
-        '''draw the prices on the board'''
-        colour = Qt.transparent # empty square could be modeled with transparent pieces
+        """draw the prices on the board"""
+        blackNo = 0
+        whiteNo = 0
         for row in range(0, len(self.boardArray)):
             for col in range(0, len(self.boardArray[0])):
-                painter.save()
-                painter.translate()
+                if self.boardArray[row][col] == 1:
+                    painter.setPen(QPen(Qt.transparent, 2, Qt.SolidLine))
+                    painter.setBrush(QBrush(Qt.white, Qt.SolidPattern))
+                    whiteNo += 1
+                elif self.boardArray[row][col] == 2:
+                    painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+                    painter.setBrush(QBrush(Qt.black, Qt.SolidPattern))
+                    blackNo += 1
+                else:
+                    painter.setPen(QPen(Qt.transparent, 2, Qt.SolidLine))
+                    painter.setBrush(QBrush(Qt.transparent, Qt.SolidPattern))
 
-                # TODO draw some the pieces as ellipses
-                # TODO choose your colour and set the painter brush to the correct colour
-                radius = (self.squareWidth() - 2) / 2
-                center = QPoint(radius, radius)
+                painter.save()
+                colTransformation = self.squareWidth() * col
+                rowTransformation = self.squareHeight() * row
+                painter.translate(colTransformation, rowTransformation)
+
+                radius = (self.squareWidth() * 0.55) / 2
+                center = QPoint(35, 35)
                 painter.drawEllipse(center, radius, radius)
                 painter.restore()
+
+    # Checks to see if the board is filled
+    def boardFilled(self):
+        for row in self.boardArray:
+            for i in row:
+                if i == 0:
+                    return False
+        return True
+
+    def showNotification(self, message):
+        QMessageBox.about(self, "!", message)
+
+    # Can be moved to game logic if needed
+    def changePlayerTurn(self):
+        # function to swap turns
+        self.counter = 20  # reset timer every turn
+        print(" -- Next Players Turn -- ")
+        if self.playerTurn == Piece.Black:
+            self.playerTurn = Piece.White
+        else:
+            self.playerTurn = Piece.Black
+
+        self.changePlayerTurnSignal.emit(self.playerTurn)  # signal sent to display Current PLayer Turn message
